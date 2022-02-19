5c2577379e3ed95c6179350b4df9d1afcbcb941c
Eason Zhao <yizhev@gmail.com>
2022-02-19 16:45:52 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '816eb48c9061437ca2ab07dad7ca981f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '青年小哲',
+  domain: 'blog.yizhe.me',
+  author: '青年小哲',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: '给时光以生命，给岁月以文明',
+  socialImageTitle: '青年小哲',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  twitter: 'yizhev',
+  github: 'YIZHEVt',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
